0841837d80eb371e455c5332c793213399c9d287
Zurdi <34356590+zurdi15@users.noreply.github.com>
2018-03-06 18:50:49 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,17 @@
 
 # Navigation Bot uninstaller
 
-echo -e "\e[36mUninstalling dependencies...\e[0m"
+RED="\e[91"
+BLUE="\e[36m"
+GREEN="\e[92m"
+NC="\e[0m"
+
+echo -e "${BLUE}Uninstalling dependencies...${NC}"
 sudo -H pip uninstall -y selenium
 sudo -H pip uninstall -y ply
 sudo -H pip uninstall -y browsermob-proxy
 
-echo -e "\e[36mUninstalling drivers...\e[0m"
+echo -e "${BLUE}Uninstalling drivers...${NC}"
 sudo rm /usr/bin/geckodriver
 echo "Geckodriver removed!"
 sudo rm /usr/bin/chromedriver
@@ -18,4 +23,4 @@ echo "Chromedriver removed!"
 rm ~/.local/share/applications/navigation-bot.desktop
 echo "Menu entry removed!"
 
-echo -e "\n\e[92mNavigation Bot succesfully uninstalled!!\e[0m\n"
+echo -e "\n${GREEN}Navigation Bot succesfully uninstalled!!${NC}\n"
