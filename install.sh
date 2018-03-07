5d06288afec11083aefe0b2c86af59a1948cacc3
Zurdi <34356590+zurdi15@users.noreply.github.com>
2018-03-07 15:33:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 #
 # Author: <Zurdi>
 
-# Navigation Bot installer
+# NBZ installer
 
-# To let the NB work, it is necesary to install three python libraries:
+# To let the NBZ work, it is necesary to install three python libraries:
 #	- selenium
 #	- browsermob-proxy
 # 	- ply
 #
-# Also we need to install some web browser drivers in /opt/navigation_bot:
+# Also we need to install some web browser drivers in /opt/nbz:
 # 	- chromedriver (Google Chrome / Chromium)
 #	- geckodriver (Firefox)
 
@@ -17,9 +17,9 @@
 PYTHON=$(which python)
 JAVA=$(which java)
 PIP=$(which pip)
-NB_INST_PATH="/opt/navigation_bot"
-NB_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DRIVERS_PATH=${NB_PATH}/lib/drivers
+NBZ_INST_PATH="/opt/nbz"
+NBZ_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DRIVERS_PATH=${NBZ_PATH}/lib/drivers
 RED="\e[91"
 BLUE="\e[36m"
 GREEN="\e[92m"
@@ -27,37 +27,37 @@ NC="\e[0m"
 
 if [ -z "$PYTHON" ]
 then
-	echo -e "${RED}Navigation Bot - Error: Python is not installed. Please install python 2.7 in your system.${NC}"
-	echo -e "${RED}Navigation Bot - Error: (sudo apt install python2.7)${NC}"
+	echo -e "${RED}NBZ - Error: Python is not installed. Please install python 2.7 in your system.${NC}"
+	echo -e "${RED}NBZ - Error: (sudo apt install python2.7)${NC}"
 	exit 1
 fi
 
 if [ -z "$JAVA" ]
 then
-	echo -e "${RED}Navigation Bot - Error: Java is not installed. Please install it in your system.${NC}"
-	echo -e "${RED}Navigation Bot - Error: (sudo apt install openjdk-9-jdk)${NC}"
+	echo -e "${RED}NBZ - Error: Java is not installed. Please install it in your system.${NC}"
+	echo -e "${RED}NBZ - Error: (sudo apt install openjdk-9-jdk)${NC}"
 	exit 1
 fi
 
 if [ -z "$PIP" ]
 then
-	echo -e "${RED}Navigation Bot - Error: Python-pip is not installed. Please install it in your system.${NC}"
-	echo -e "${RED}Navigation Bot - Error: (sudo apt install python-pip)${NC}"
+	echo -e "${RED}NBZ - Error: Python-pip is not installed. Please install it in your system.${NC}"
+	echo -e "${RED}NBZ - Error: (sudo apt install python-pip)${NC}"
 	exit 1
 fi
 
-if [ ! -d $NB_INST_PATH ]
+if [ ! -d $NBZ_INST_PATH ]
 then
-	sudo mkdir $NB_INST_PATH
+	sudo mkdir $NBZ_INST_PATH
 fi
 
 
-for element in $(ls ${NB_PATH})
+for element in $(ls ${NBZ_PATH})
 do
-	sudo cp -r $element $NB_INST_PATH 
+	sudo cp -r $element $NBZ_INST_PATH 
 done
 
-sudo chown -R $(whoami):$(whoami) $NB_INST_PATH
+sudo chown -R $(whoami):$(whoami) $NBZ_INST_PATH
 
 # Installing libraries
 echo -e "${BLUE}Installing dependencies...${NC}"
@@ -73,10 +73,10 @@ sudo cp ${DRIVERS_PATH}/chromedriver /usr/bin
 echo "Chromedriver installed in /usr/bin!"
 
 # Creating menu entry
-menu_entry="[Desktop Entry]\nVersion=1.0\nType=Application\nName=Navigation Bot\nGenericName=navigation_bot\nIcon=${NB_INST_PATH}/nb_icon.png\nExec=${NB_INST_PATH}/navigation_bot_launcher.sh -s ${NB_INST_PATH}/scripts/bdfutbol.nbz\nPath=${NB_INST_PATH}\nNoDisplay=False\nCategories=Development;\nStartupNotify=false\nTerminal=true"
+menu_entry="[Desktop Entry]\nVersion=1.0\nType=Application\nName=NBZ\nGenericName=NBZ\nIcon=${NBZ_INST_PATH}/nbz_icon.png\nExec=${NBZ_INST_PATH}/nbz_launcher.sh -s ${NBZ_INST_PATH}/scripts/bdfutbol.nbz\nPath=${NBZ_INST_PATH}\nNoDisplay=False\nCategories=Development;\nStartupNotify=false\nTerminal=true"
 
-echo -e $menu_entry > ~/.local/share/applications/navigation-bot.desktop
+echo -e $menu_entry > ~/.local/share/applications/nbz.desktop
 
 echo "Menu entry created!"
 
-echo -e "\n${GREEN}Navigation Bot installed succesfully!!${NC}\n"
+echo -e "\n${GREEN}NBZ installed succesfully!!${NC}\n"
