c302630f2f6013c3876d11d97666093f6c3a6ffb
Zurdi <34356590+zurdi15@users.noreply.github.com>
2018-05-28 10:27:48 +0200

    Update lib_snf_nbz.py

--- lib/lib_snf_nbz.py
+++ lib/lib_snf_nbz.py
@@ -16,16 +16,16 @@ class LibSnf:
         pass
 
 
-    def check_net(har, request):
+    def check_net(self, har, request):
         if request[0] == 'params':
             return self.check_net_parameters(har, request)
         elif request[0] == 'keyword':
-            return self.check_net_keyword(har, request)
+            return self.check_net_keywords(har, request)
         else:
             logger.log('ERROR', 'Not admited request type: ' + request[0])
     
     
-    def check_net_parameters(har, request):
+    def check_net_parameters(self, har, request):
         """
         Check if any request had choosen parameters
         Return selected value from any request
@@ -68,7 +68,7 @@ class LibSnf:
             logger.log('ERROR', 'Can\'t find ' + str(value) + ' - invalid search')
             sys.exit(-1)
     
-    def check_net_keyword(har, request):
+    def check_net_keywords(self, har, request):
         """
         Check if any request had choosen url
         Return selected value from any request
@@ -80,14 +80,21 @@ class LibSnf:
         timestamp = ''
     
         value = request[1] # Value to return
+        keywords = request[2:] # Keywords to search
     
         for entry in har['log']['entries']:
-            if not entry['request']['url'].find(request[1]) == -1:
-                request_ok = True
-                status_code = int(entry['response']['status'])
-                url = entry['request']['url']
-                timestamp = entry['startedDateTime'].replace('T', ' ')[:-10]
-                break
+            for keyword in keywords:
+                if entry['request']['url'].find(keyword) != -1:
+                    request_ok = True
+                    status_code = int(entry['response']['status'])
+                    url = entry['request']['url']
+                    timestamp = entry['startedDateTime'].replace('T', ' ')[:-10]
+                else:
+                    request_ok = False
+                    url = ''
+                    status_code = 404
+                    timestamp = ''
+                    break
     
         if value == 'request_ok':
             return request_ok
