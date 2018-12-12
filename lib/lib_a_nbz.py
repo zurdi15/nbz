742425395daab9d37f3536a94c7fd4667b445fef
Zurdi <34356590+zurdi15@users.noreply.github.com>
2018-12-12 09:42:08 +0100

    Update lib_a_nbz.py

--- lib/lib_a_nbz.py
+++ lib/lib_a_nbz.py
@@ -5,6 +5,7 @@
 
 
 import time
+import datetime
 from datetime import datetime
 from random import randint
 import re
@@ -110,13 +111,17 @@ class LibA:
 			Actual system timestamp string
 		"""
 
-		date_format = params[0]
 		try:
-			if not date_format:
-				return time.strftime("%Y-%m-%d %H:%M:%S")
-			else:
-				return time.strftime(date_format)
-		except Exception as e:
+			date_format = params[0]
+		except:
+			date_format = False
+			logger.log('NOTE', 'Using default timestamp format')
+		try:
+                        if not date_format:
+				return str(datetime.now())[:-3]
+                        else:
+                                return time.strftime(date_format)
+                except Exception as e:
 			raise Exception('Error getting actual timestamp: {exception}'.format(exception=e))
 
 	@staticmethod
