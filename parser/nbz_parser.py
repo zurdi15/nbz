8d555b2574675c028a9816f05fcd4219c07381c0
zurdi15 <zurdizurdo25@gmail.com>
2019-03-21 17:12:52 +0100

    some cleaner parser

--- parser/nbz_parser.py
+++ parser/nbz_parser.py
@@ -7,7 +7,7 @@
 from lib.lib_log_nbz import Logging
 from data.natives import NATIVES
 try:
-	from nbz_lexer import tokens  # Get the token map from the lexer
+	from nbz_lexer import tokens
 	import ply.yacc as yacc
 except LookupError:
 	raise Exception("Dependencies not installed. Please run setup.sh.")
@@ -30,20 +30,20 @@ def NBZParser(script, interactive=False):
 		A dict mapping variables of the script and their values
 	"""
 
-	# z_code structure
-	z_code = []
+	# code structure
+	code = []
 
-	# z_code_vars dictionary
-	z_code_vars = {}
+	# variables dictionary
+	variables = {}
 
 	# Functions dictionary
 	functions = NATIVES
 
 	# Initial state
-	def p_sent_list(p):
-		"""sent_list : sent_list sent
-					 | sent
-					 | empty"""
+	def p_code(p):
+		"""code : code sent
+				| sent
+				| empty"""
 		if len(p) == 2:
 			if p[1] is None:
 				p[0] = []
@@ -54,60 +54,147 @@ def NBZParser(script, interactive=False):
 			p[0].append(p[2])
 
 	def p_sent(p):
-		"""sent : sent_func_def
-				| sent_assign
-				| sent_func SEMI"""
+		"""sent : statement
+				| instruction SEMI"""
 		p[0] = p[1]
 
-	# Functions definition
-	def p_sent_funcs_def(p):
-		"""sent_func_def : DEF ID LPAREN RPAREN LBRACE sent_list RBRACE"""
+	def p_instruction(p):
+		"""instruction : assign
+					   | function"""
+		p[0] = p[1]
+
+	def p_statement(p):
+		"""statement : function_definition
+					 | for_statement
+					 | if_statement
+					 | while_statement"""
+		p[0] = p[1]
+
+	def p_sent_for_flow_int(p):
+		"""for_statement : FOR LPAREN for_valid_expr COMMA for_valid_expr COMMA for_valid_iter RPAREN LBRACE code RBRACE
+					 	 | FOR LPAREN ID IN ID RPAREN LBRACE code RBRACE"""
+		if len(p) == 10:
+			print 'for'
+			p[0] = ['for', p[3], p[5], p[8]]
+			for i in range(0, len(p[8])):
+				code.pop()
+			code.append(p[0])
+		else:
+			p[0] = ['for', p[3], p[5], p[7], p[10]]
+			for i in range(0, len(p[10])):
+				code.pop()
+			code.append(p[0])
+
+	def p_for_valid_expressions_num(p):
+		"""for_valid_expr : expr_num
+						  | expr_arithm"""
+		p[0] = p[1]
+
+	def p_for_valid_iterators(p):
+		"""for_valid_iter : PLUS
+						  | PLUSPLUS
+						  | MINUS
+						  | MINUSMINUS"""
+		p[0] = p[1]
+
+	def p_sent_if_flow(p):
+		"""if_statement : IF LPAREN logic_list RPAREN LBRACE code RBRACE
+			  			| IF LPAREN logic_list RPAREN LBRACE code RBRACE elif_sent
+			  			| IF LPAREN logic_list RPAREN LBRACE code RBRACE ELSE LBRACE code RBRACE
+			  			| IF LPAREN logic_list RPAREN LBRACE code RBRACE elif_sent ELSE LBRACE code RBRACE"""
+		if len(p) == 8:  # Only if
+			p[0] = ['if', p[3], p[6]]
+			for i in range(0, len(p[6])):
+				code.pop()
+			code.append(p[0])
+		elif len(p) == 9:  # If + elif
+			p[0] = ['if', p[3], p[6], p[8]]
+			for i in range(0, len(p[6])):
+				code.pop()
+			code.append(p[0])
+		elif len(p) == 12:  # If + else
+			p[0] = ['if', p[3], p[6], [['else'] + [p[10]]]]
+			for i in range(0, len(p[6])):
+				code.pop()
+			for i in range(0, len(p[10])):
+				code.pop()
+			code.append(p[0])
+		elif len(p) == 13:  # If + elif + else
+			if p[8]:
+				p[0] = ['if', p[3], p[6], p[8], [['else'] + [p[11]]]]
+			else:
+				p[0] = ['if', p[3], p[6], [['else'] + [p[11]]]]
+			for i in range(0, len(p[6])):
+				code.pop()
+			for i in range(0, len(p[11])):
+				code.pop()
+			code.append(p[0])
+
+	def p_sent_elif_flow(p):
+		"""elif_sent : ELIF LPAREN logic_list RPAREN LBRACE code RBRACE elif_sent
+					 | empty"""
+		if len(p) > 2:
+			for i in range(0, len(p[6])):
+				code.pop()
+			if p[8]:
+				p[0] = [['elif', p[3], p[6]], p[8][0]]
+			else:
+				p[0] = [['elif', p[3], p[6]]]
+
+	def p_sent_while_flow(p):
+		"""while_statement : WHILE LPAREN logic_list RPAREN LBRACE code RBRACE"""
+		p[0] = ['while', p[3], p[6]]
+		for i in range(0, len(p[6])):
+			code.pop()
+		code.append(p[0])
+
+	def p_function_definition(p):
+		"""function_definition : DEF ID LPAREN RPAREN LBRACE code RBRACE"""
 		functions[p[2]] = ''
 		p[0] = ['def', p[2], p[6]]
 		for sent in p[6]:
-			z_code.pop()
-		z_code.append(p[0])
+			code.pop()
+		code.append(p[0])
 
-	# Assign definitions
-	def p_sent_assign_expr(p):
-		"""sent_assign : ID ASSIGN expr_type SEMI
-					   | ID ASSIGN expr_arithm SEMI
-					   | ID ASSIGN logic_list SEMI
-					   | ID ASSIGN expr_list SEMI"""
+	def p_assign_expr(p):
+		"""assign : ID ASSIGN expr_type 
+				  | ID ASSIGN expr_arithm 
+				  | ID ASSIGN logic_list 
+				  | ID ASSIGN expr_list"""
 		p[0] = ['assign', p[1], p[3]]
-		z_code_vars[p[1]] = ''
-		z_code.append(p[0])
+		variables[p[1]] = ''
+		code.append(p[0])
 
-	def p_sent_assign_func(p):
-		"""sent_assign : ID ASSIGN sent_func SEMI"""
-		z_code.pop()
+	def p_assign_func(p):
+		"""assign : ID ASSIGN function"""
+		code.pop()
 		p[0] = ['assign', p[1], p[3]]
-		z_code_vars[p[1]] = ''
-		z_code.append(p[0])
+		variables[p[1]] = ''
+		code.append(p[0])
 
-	# Function call expression
 	def p_expr_funcs(p):
-		"""sent_func : ID LPAREN list RPAREN"""
+		"""function : ID LPAREN list RPAREN"""
 		try:
 			check = functions[p[1]]
 			p[0] = ['func', p[1], p[3]]
-			z_code.append(p[0])
+			code.append(p[0])
 		except LookupError:
 			raise Exception('Undefined function "{function}" line {line}'.format(function=p[1],
 																				 line=p.lineno(1)))
 
-	# List expressions
 	def p_list_var(p):
 		"""list : list COMMA ID
-				| list COMMA sent_func
-				| sent_func
+				| list COMMA function
+				| function
 				| ID"""
 		if len(p) == 2:
 			if isinstance(p[1], str):
 				try:
-					check = z_code_vars[p[1]]
+					print 'check'
+					check = variables[p[1]]
 					p[0] = [['var', p[1]]]
 				except LookupError:
+					print 'error'
 					raise Exception('Undefined variable "{variable}" line {line}'.format(variable=p[1],
 																						 line=p.lineno(1)))
 
@@ -115,7 +202,7 @@ def NBZParser(script, interactive=False):
 				try:
 					check = functions[p[1][1]]
 					p[0] = [p[1]]
-					z_code.pop()
+					code.pop()
 				except LookupError:
 					raise Exception('Undefined function "{function}" line {line}'.format(function=p[1][1],
 																						 line=p.lineno(1)))
@@ -124,7 +211,7 @@ def NBZParser(script, interactive=False):
 			p[0] = p[1]
 			if isinstance(p[3], str):
 				try:
-					check = z_code_vars[p[3]]
+					check = variables[p[3]]
 					p[0].append(['var', p[3]])
 				except LookupError:
 					raise Exception('Undefined variable "{variable}" line {line}'.format(variable=p[3],
@@ -134,7 +221,7 @@ def NBZParser(script, interactive=False):
 				try:
 					check = functions[p[3][1]]
 					p[0].append([p[3]])
-					z_code.pop()
+					code.pop()
 				except LookupError:
 					raise Exception('Undefined function "{function}" line {line}'.format(function=p[3][1],
 																						 line=p.lineno(1)))
@@ -163,91 +250,6 @@ def NBZParser(script, interactive=False):
 			p[0] = p[1]
 			p[0].append(p[3])
 
-	# Flow control expressions
-
-	# For
-	def p_sent_for_flow_int(p):
-		"""sent : FOR LPAREN for_valid_expr COMMA for_valid_expr COMMA for_valid_iter RPAREN LBRACE sent_list RBRACE
-				| FOR LPAREN ID IN ID RPAREN LBRACE sent_list RBRACE"""
-		if len(p) == 10:
-			p[0] = ['for', p[3], p[5], p[8]]
-			z_code_vars[p[3]] = ''
-			for i in range(0, len(p[8])):
-				z_code.pop()
-			z_code.append(p[0])
-		else:
-			p[0] = ['for', p[3], p[5], p[7], p[10]]
-			for i in range(0, len(p[10])):
-				z_code.pop()
-			z_code.append(p[0])
-
-	def p_for_valid_expressions_num(p):
-		"""for_valid_expr : expr_num
-						  | expr_arithm"""
-		p[0] = p[1]
-
-	def p_for_valid_iterators(p):
-		"""for_valid_iter : PLUS
-						  | PLUSPLUS
-						  | MINUS
-						  | MINUSMINUS"""
-		p[0] = p[1]
-
-	# If / else
-	def p_sent_if_flow(p):
-		"""sent : IF LPAREN logic_list RPAREN LBRACE sent_list RBRACE
-				| IF LPAREN logic_list RPAREN LBRACE sent_list RBRACE elif_sent
-				| IF LPAREN logic_list RPAREN LBRACE sent_list RBRACE ELSE LBRACE sent_list RBRACE
-				| IF LPAREN logic_list RPAREN LBRACE sent_list RBRACE elif_sent ELSE LBRACE sent_list RBRACE"""
-		if len(p) == 8:  # Only if
-			p[0] = ['if', p[3], p[6]]
-			for i in range(0, len(p[6])):
-				z_code.pop()
-			z_code.append(p[0])
-		elif len(p) == 9:  # If + elif
-			p[0] = ['if', p[3], p[6], p[8]]
-			for i in range(0, len(p[6])):
-				z_code.pop()
-			z_code.append(p[0])
-		elif len(p) == 12:  # If + else
-			p[0] = ['if', p[3], p[6], [['else'] + [p[10]]]]
-			for i in range(0, len(p[6])):
-				z_code.pop()
-			for i in range(0, len(p[10])):
-				z_code.pop()
-			z_code.append(p[0])
-		elif len(p) == 13:  # If + elif + else
-			if p[8]:
-				p[0] = ['if', p[3], p[6], p[8], [['else'] + [p[11]]]]
-			else:
-				p[0] = ['if', p[3], p[6], [['else'] + [p[11]]]]
-			for i in range(0, len(p[6])):
-				z_code.pop()
-			for i in range(0, len(p[11])):
-				z_code.pop()
-			z_code.append(p[0])
-
-	# Elif
-	def p_sent_elif_flow(p):
-		"""elif_sent : ELIF LPAREN logic_list RPAREN LBRACE sent_list RBRACE elif_sent
-					 | empty"""
-		if len(p) > 2:
-			for i in range(0, len(p[6])):
-				z_code.pop()
-			if p[8]:
-				p[0] = [['elif', p[3], p[6]], p[8][0]]
-			else:
-				p[0] = [['elif', p[3], p[6]]]
-
-	# While
-	def p_sent_while_flow(p):
-		"""sent : WHILE LPAREN logic_list RPAREN LBRACE sent_list RBRACE"""
-		p[0] = ['while', p[3], p[6]]
-		for i in range(0, len(p[6])):
-			z_code.pop()
-		z_code.append(p[0])
-
-	# Logic list
 	def p_group_logic_list(p):
 		"""logic_list : LPAREN logic_list RPAREN"""
 		p[0] = p[2]
@@ -264,7 +266,6 @@ def NBZParser(script, interactive=False):
 			elif p[2] == 'or':
 				p[0] = ['boolean', p[1], p[3], 'or']
 
-	# Boolean expressions
 	def p_expr_logical(p):
 		"""expr_bool : expr_bool EQ expr_bool
 					 | expr_bool LT expr_bool
@@ -289,10 +290,10 @@ def NBZParser(script, interactive=False):
 			p[0] = ['boolean', p[2], p[2], 'not']
 
 	def p_logic_valid_var(p):
-		"""expr_bool : sent_func"""
+		"""expr_bool : function"""
 		try:
 			check = functions[p[1][1]]
-			z_code.pop()
+			code.pop()
 			p[0] = p[1]
 		except LookupError:
 			raise Exception('Undefined function "{function}" line {line}'.format(function=p[1][1],
@@ -303,7 +304,6 @@ def NBZParser(script, interactive=False):
 					 | expr_arithm"""
 		p[0] = p[1]
 
-	# Arithmethic expressions
 	def p_group_expr_arithmethic(p):
 		"""expr_arithm : LPAREN expr_arithm RPAREN"""
 		p[0] = p[2]
@@ -327,10 +327,10 @@ def NBZParser(script, interactive=False):
 
 	def p_arithm_valid_var(p):
 		"""expr_arithm : ID
-					   | sent_func"""
+					   | function"""
 		if isinstance(p[1], str):
 			try:
-				check = z_code_vars[p[1]]
+				check = variables[p[1]]
 				p[0] = ['var', p[1]]
 			except LookupError:
 				raise Exception('Undefined variable "{variable}" line {line}'.format(variable=p[1],
@@ -339,7 +339,7 @@ def NBZParser(script, interactive=False):
 		elif isinstance(p[1], list):
 			try:
 				check = functions[p[1][1]]
-				z_code.pop()
+				code.pop()
 				p[0] = p[1]
 			except LookupError:
 				raise Exception('Undefined function "{function}" line {line}'.format(function=p[1][1],
@@ -349,9 +349,8 @@ def NBZParser(script, interactive=False):
 		"""expr_arithm : expr_type"""
 		p[0] = p[1]
 
-	# Type definitions
 	def p_sent_index_list(p):
-		"""sent_func : sent_index_list"""
+		"""function : sent_index_list"""
 		p[0] = p[1]
 
 	def p_index_list_var(p):
@@ -359,22 +358,22 @@ def NBZParser(script, interactive=False):
 						   | ID LBRACKET ID RBRACKET"""
 		if not isinstance(p[1], list):
 			try:
-				check = z_code_vars[p[1]]
+				check = variables[p[1]]
 			except LookupError:
 				raise Exception('Undefined list "{list}" line {line}'.format(list=p[1],
 																			 line=p.lineno(1)))
 
 			try:
-				check = z_code_vars[p[3]]
+				check = variables[p[3]]
 			except LookupError:
 				raise Exception('Undefined variable "{variable}" line {line}'.format(variable=p[3],
 																					 line=p.lineno(1)))
 
 			p[0] = ['func', 'get_element_list', [['var', p[1]], ['var', p[3]]]]
-			z_code.append(p[0])
+			code.append(p[0])
 		else:
 			try:
-				check = z_code_vars[p[3]]
+				check = variables[p[3]]
 			except LookupError:
 				raise Exception('Undefined variable "{variable}" line {line}'.format(variable=p[3],
 																					 line=p.lineno(1)))
@@ -386,9 +385,9 @@ def NBZParser(script, interactive=False):
 						   | ID LBRACKET INTEGER RBRACKET"""
 		if not isinstance(p[1], list):
 			try:
-				check = z_code_vars[p[1]]
+				check = variables[p[1]]
 				p[0] = ['func', 'get_element_list', [['var', p[1]], p[3]]]
-				z_code.append(p[0])
+				code.append(p[0])
 			except LookupError:
 				raise Exception('Undefined list "{list}" line {line}'.format(list=p[1],
 																			 line=p.lineno(1)))
@@ -467,7 +466,7 @@ def NBZParser(script, interactive=False):
 			parser.parse(data)
 		except EOFError:
 			raise Exception('General error parsing {script}'.format(script=script))
-		return z_code, z_code_vars
+		return code, variables
 	else:
 		while True:
 			try:
