e9798a68e8289dc433b07d370df498b07c766ea9
Alejandro Atance Fernandez <Alejandro.Atance@unidadeditorial.es>
2019-06-25 13:03:22 +0200

    unicode cleaning

--- nbz_core.py
+++ nbz_core.py
@@ -252,29 +252,23 @@ class NBZCore:
 					elif sub_instruction[0] == 'value':
 						return sub_instruction[1]
 					elif sub_instruction[0] == 'arithm':
-						if sub_instruction[3] == '+':
-							op_1 = self.get_value(sub_instruction[1])
-							op_2 = self.get_value(sub_instruction[2])
-							if isinstance(op_1, str) or isinstance(op_2, str):
-								if isinstance(op_1, unicode):
-									op_1 = op_1.encode('utf-8')
-								if isinstance(op_1, unicode):
-									op_2 = op_2.encode('utf-8')
-								return '{op_1}{op_2}'.format(op_1=str(op_1),
-															 op_2=str(op_2))
-							else:
-								return op_1 + op_2
+						op_1 = self.get_value(sub_instruction[1])
+						op_2 = self.get_value(sub_instruction[2])
+						if isinstance(op_1, str) or isinstance(op_2, str):
+							return eval('\'{op_1}\' {operand} \'{op_2}\''.format(op_1=self.get_value(sub_instruction[1]),
+																				operand=sub_instruction[3],
+																				op_2=self.get_value(sub_instruction[2])))	
 						else:
-							return eval('{op_1}{operand}{op_2}'.format(op_1=self.get_value(sub_instruction[1]),
-																	   operand=sub_instruction[3],
-																	   op_2=self.get_value(sub_instruction[2])))
+							return eval('{op_1} {operand} {op_2}'.format(op_1=self.get_value(sub_instruction[1]),
+																		operand=sub_instruction[3],
+																		op_2=self.get_value(sub_instruction[2])))
 					elif sub_instruction[0] == 'boolean':
 						if sub_instruction[3] != 'not':
 							op_1 = self.get_value(sub_instruction[1])
 							op_2 = self.get_value(sub_instruction[2])
-							if isinstance(op_1, str) or isinstance(op_1, unicode):
+							if isinstance(op_1, str):
 								op_1 = "'{op_1}'".format(op_1=op_1)
-							if isinstance(op_2, str) or isinstance(op_2, unicode):
+							if isinstance(op_2, str):
 								op_2 = "'{op_2}'".format(op_2=op_2)
 							return eval('{op_1} {operand} {op_2}'.format(op_1=self.get_value(op_1),
 																		operand=sub_instruction[3],
